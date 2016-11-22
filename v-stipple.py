e6a671f8c85b31881a23f0db528f38b3430e1367
jxmorris12 <jxmorris12@gmail.com>
2016-11-21 22:27:57 -0500

    preliminary voronoi stippling

--- v-stipple.py
+++ v-stipple.py
@@ -2,78 +2,81 @@
 # jack morris 11/08/16
 #
 
-
-from operator import itemgetter
 from PIL import Image
+import random
+import math
+ 
+def generate_voronoi_diagram(num_cells):
+  image = Image.open("pics/uva-logo.png").convert("L") 
+  #image = Image.new("RGB", (width, height))
+  pixels = image.load()
+  putpixel = image.putpixel
+  imgx, imgy = image.size
+  #
+  nx = []
+  ny = []
+  nr = []
+  #
+  region_data = {}
+  # random seed points
+  for i in range(num_cells):
+    nx.append(random.randrange(imgx))
+    ny.append(random.randrange(imgy))
+    nr.append(random.randrange(256))
+  # compute centroids
+  for y in range(imgy):
+    for x in range(imgx):
+      dmin = math.hypot(imgx-1, imgy-1) * 1
+      j = -1
+      for i in range(num_cells):
+          d = math.hypot(nx[i]-x, ny[i]-y) * (1-pixels[x,y]/255.)
+          if d < dmin:
+            dmin = d
+            j = i
+      region_data[x,y] = j
+      putpixel((x, y), 0) # clear image for now
+  # find centroids
+  centroids = []
+  print "0.0%"
+  for i in range(num_cells):
+    print str((i+1)*1.0/num_cells*100)+"%"
+    start_pt = (nx[i],ny[i])
+    q = [start_pt]
+    cx = 0
+    cy = 0
+    ct = 0.0
+    j = 0
+    while j < len(q):
+      pt = q[j]
+      j += 1
+      nn_x, nn_y = pt
+      if region_data[pt] == region_data[start_pt]:
+        # add to centroid
+        cx += nn_x * 1 # rho = 1 for now
+        cy += nn_y * 1 # " 
+        ct += 1
+        # BFS to continue queue
+        for cn in cardinal_neighbors(pt):
+            if in_bounds(cn, image.size) and cn not in q:
+                q.append(cn)
+    if ct == 0: 
+     continue
+    cx /= ct
+    cy /= ct
+    centroids += [(int(cx), int(cy))]
+  # mark centroids
+  print "Number of cells:", num_cells, "Number of centroids:", len(centroids)
+  for c in centroids:
+    print "Putting pixel at centroid:", c
+    putpixel(c, 255)
+  # save
+  image.save("VoronoiDiagram.png", "PNG")
+  image.show()
 
+def in_bounds(point, size):
+    return (point[0] >= 0 and point[0] < size[0]) and (point[1] >= 0 and point[1] < size[1])
 
-#
-def stipple(img):
-  #
-  width, height = img.size
-  #
-  MAX_STIPPLE_COUNT = (width * height) / 100 * 12
-  DISTANCE_THRESHOLD = (width**2 + height**2) ** .5 / MAX_STIPPLE_COUNT * 32.0 # dumb, but this probably *SHOULD* reference the diagonal
-  #
-  pixels = img.load()
-  #
-  stipple_count = [ [0] * width for i in xrange(height) ]
-  #
-  max_counts = []
-  #
-  for i in xrange( 1, width-1 ):
-    #
-    for j in xrange( 1, height-1 ):
-      #
-      stipple_count[j][i] =  (pixels[i,j] - pixels[i-1,j])**2
-      stipple_count[j][i] += (pixels[i,j] - pixels[i,j-1])**2 
-      stipple_count[j][i] += (pixels[i,j] - pixels[i+1,j])**2
-      stipple_count[j][i] += (pixels[i,j] - pixels[i,j+1])**2
-      max_counts . append( [ stipple_count[j][i], (j,i) ] )
-      #
-    #
-  #
-  max_counts.sort(key=itemgetter(0)) # Sort by element 0 of tuple 
-  max_counts = max_counts[-MAX_STIPPLE_COUNT:]
-  #
-  # normalize
-  for i in xrange( width ):
-      for j in xrange( height ):
-          pixels[i,j] = 0
-  # mark pixels
-  marked_pixels = []
-  #
-  for x in xrange( len(max_counts) ):
-    #
-    i = max_counts[x][1][1]
-    j = max_counts[x][1][0]
-    #
-    if min_distance_from_marked_pixels(marked_pixels, i, j) >= DISTANCE_THRESHOLD:
-      # mark pixel as yes
-      pixels[i,j] = 255
-      marked_pixels.append((i,j))
-      #
-    #
-  #
-  # img.show()
-  return img
-  #
-#
-def min_distance_from_marked_pixels(marked_pixels, x, y):
-  #
-  min_distance = 10000000 # Dumb ... must be > img diagonal
-  #
-  for a in xrange(len(marked_pixels)):
-    #
-    i = marked_pixels[a][0]
-    j = marked_pixels[a][1]
-    #
-    distance = ((x-i)**2 + (y-j)**2)**.5
-    if distance < min_distance:
-      #
-      min_distance = distance
-      #
-  #
-  return min_distance
-  #
-#
+def cardinal_neighbors(pt):
+    return [(pt[0]+1,pt[1]), (pt[0],pt[1]-1), (pt[0]-1,pt[1]),(pt[0],pt[1]+1)]
+
+generate_voronoi_diagram(50)
