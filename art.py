e7632bffed62af9e02efede25a67037cb6c63a94
jxmorris12 <jxmorris12@gmail.com>
2016-11-23 13:16:32 -0500

    successful greedy implementation

--- art.py
+++ art.py
@@ -2,21 +2,26 @@
 # jack morris 11/15/16
 #
 
-import tsp
-import stipple as stippler
 from PIL import Image
+import vstipple as stippler
+import greedy_tsp as tsp
 
 def __main__():
     #
     # print "__main__()"
-    #image_filename = "homer.jpg"
-    image_filename = "uva-logo.png"
-    #image_filename = "smiley-grad.png"
-    #image_filename = "cross.jpg"
+    #image_filename = "pics/homer.jpg"
+    image_filename = "pics/uva-logo.png"
+    #image_filename = "pics/smiley-grad.png"
+    #image_filename = "pics/cross.jpg"
     image = Image.open(image_filename).convert('L')
-    stippled_image = stippler.stipple(image)
+    print "Stippling image..."
+    stippled_image = stippler.voronoi_stipple(image)
+    stippled_image.show()
+    print "Connecting image dots..."
     tsp_image = tsp.connect_the_dots(stippled_image)
-    tsp_image.show ()
+    tsp_image.show()
+    # export
+    tsp_image.save("tsp.png", "PNG")
     #
 
 if __name__ == "__main__":
